be0df7d8e8892191e152acaa01b01f88898cb761
atempa <monserrath.atempa@uabc.edu.mx>
2026-09-17 20:50:37 -0700

    commit

--- entidad/src/main/java/mx/sauap/entidad/ConsultaDeLasAsignacion.java
+++ entidad/src/main/java/mx/sauap/entidad/ConsultaDeLasAsignacion.java
@@ -1,4 +1,35 @@
 package mx.sauap.entidad;
 
-public class ConsultaDeLasAsignacion {
+import jakarta.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Profesor_has_Unidades_De_Aprendizaje", schema = "sauap_proyecto")
+@IdClass(ConsultaDeLasAsignacionPK.class)
+public class ConsultaDeLasAsignacion implements Serializable {
+
+    @Id
+    @Column(name = "id_Clase")
+    private Integer idClase;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Profesor_id_Profesor", nullable = false)
+    private profesorUA profesor;
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "Unidades_De_Aprendizaje_id_Unidades_De_Aprendizaje", nullable = false)
+    private unidadesDeAprendizaje unidadDeAprendizaje;
+
+    public ConsultaDeLasAsignacion() {}
+
+    public Integer getIdClase() { return idClase; }
+    public void setIdClase(Integer idClase) { this.idClase = idClase; }
+
+    public profesorUA getProfesor() { return profesor; }
+    public void setProfesor(profesorUA profesor) { this.profesor = profesor; }
+
+    public unidadesDeAprendizaje getUnidadDeAprendizaje() { return unidadDeAprendizaje; }
+    public void setUnidadDeAprendizaje(unidadesDeAprendizaje unidadDeAprendizaje) { this.unidadDeAprendizaje = unidadDeAprendizaje; }
 }
--- entidad/src/main/java/mx/sauap/entidad/Profesor.java
+++ entidad/src/main/java/mx/sauap/entidad/Profesor.java
@@ -1,9 +1,24 @@
 package mx.sauap.entidad;
 
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.list;
 
 /*esta es la clase de entidad de Profesor,
 aqui especifique los parametros*/
 
-public class Profesor {
+@Entity
+@Table(name= "Profesor", schema = "sauap_proyecto")
+public class Profesor implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "id_Profesor")
+    private Integer idProfesor;
+
+    @Column(name= "nombre_Profesor", length=50, nullable = false)
+    private String nombreProfesor;
+
+    @Column(name= "apellido_paterno", length=50, nullable = false)
+    private String nombreProfesor;
 
 }
